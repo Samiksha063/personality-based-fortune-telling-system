28217c0e850aed6385b60fc952cdcf9dac96ca13
Samiksha063 <samiksha3stha@gmail.com>
2026-06-13 11:50:41 +0545

    add take_test function

--- fortune.py
+++ fortune.py
@@ -2,7 +2,41 @@
 # group members : samiksha shrestha and bina maya ghale
 
 #take test -> asks questions
-# def take_test():
+def take_test():
+    name = input("\nEnter your name: ").strip().lower()
+
+    #scores
+    scores = {
+        "caring" : 0, "social" : 0, "adventurous" : 0
+    }
+
+    #questions -> dictionary of lists
+    questions = {
+        "caring" : [
+            "Do you notice when someone in the room is feeling left out?",
+            "Would you alter your plans to support a friend going through a tough time?"
+        ],
+        "social" : [
+            "Are you usually the one to start a conversation in a group?",
+            "Do you feel energized after spending an evening at a crowded event?"
+        ],
+        "adventurous" : [
+            "Would you go on a spontaneous road trip with zero planning?",
+            "Do you actively look for thrilling activities like extreme sports or amusement parks?"
+        ]
+    }
+
+    print("\nQUESTIONS (Enter y/Y for yes and n/N for no)")
+
+    for trait in questions:
+        for question in questions[trait] :
+            answer = input(f"\n{question} (y/n): ").lower()
+
+            if answer == 'y':
+                scores[trait] += 1
+
+    personality, fortune = get_result(scores)
+    return {"name" : name, "score" : scores, "personality": personality, "fortune": fortune }
 
 #view result
 # def view_result():
