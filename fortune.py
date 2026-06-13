37ee7af8743f60a969f162b2e8779fb132e31a28
Samiksha063 <samiksha3stha@gmail.com>
2026-06-13 09:25:16 +0545

    inital setup

---
+++ fortune.py
@@ -0,0 +1,24 @@
+# project title : personality based fortune telling system 
+# group members : samiksha shrestha and bina maya ghale
+
+#take test -> asks questions
+# def take_test():
+
+#view result
+# def view_result():
+
+
+#search result
+# def search_result():
+
+
+#delete result
+# def delete_result():
+
+
+#menu
+# while True:
+#     print("\nWELCOME TO PERSONALITY BASED FORTUNE TELLING SYSTEM")
+    
+        
+
