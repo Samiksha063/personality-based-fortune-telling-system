53ebe0fae04896834a49ce3aadcdaaa3751f8637
binamayghale5 <binamayaghale5@gmail.com>
2026-06-13 10:24:35 +0545

    updated menu option

--- fortune.py
+++ fortune.py
@@ -22,32 +22,36 @@ while True:
     print("\nWELCOME TO PERSONALITY BASED FORTUNE TELLING SYSTEM")
     print("\n Lets get started !")
     print("\n----- MENU -----")
-    print("1. Take Test")
-    print("2. View Result")
-    print("3. Search Result")
-    print("4. Delete Result")
-    print("5. Exit")
+    print("\n1. Take Test")
+    print("\n2. View Result")
+    print("\n3. Search Result")
+    print("\n4. Delete Result")
+    print("\n5. Exit")
 
-    choice = input("Enter your choice: ")
+    try:
+        choice = int(input("\nEnter a number: "))
+    except ValueError:
+        print("\nInvalid input!")
+        continue
 
-    if choice == "1":
+    if choice == 1:
         take_test()
 
-    elif choice == "2":
+    elif choice == 2:
         view_result()
 
-    elif choice == "3":
+    elif choice == 3:
         search_result()
 
-    elif choice == "4":
+    elif choice == 4:
         delete_result()
 
-    elif choice == "5":
-        print("Goodbye!")
+    elif choice == 5:
+        print("\nGoodbye!")
         break
 
     else:
-        print("Invalid choice!")
+        print("\nInvalid choice!")
 
         
 
