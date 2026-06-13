a380c3a1a89d183210b9b58eea9b176f8c97dcdf
binamayghale5 <binamayaghale5@gmail.com>
2026-06-13 09:46:18 +0545

    Creating menu option

--- fortune.py
+++ fortune.py
@@ -18,7 +18,36 @@
 
 #menu
 # while True:
-#     print("\nWELCOME TO PERSONALITY BASED FORTUNE TELLING SYSTEM")
-    
+while True:
+    print("\nWELCOME TO PERSONALITY BASED FORTUNE TELLING SYSTEM")
+    print("\n Lets get started !")
+    print("\n----- MENU -----")
+    print("1. Take Test")
+    print("2. View Result")
+    print("3. Search Result")
+    print("4. Delete Result")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        take_test()
+
+    elif choice == "2":
+        view_result()
+
+    elif choice == "3":
+        search_result()
+
+    elif choice == "4":
+        delete_result()
+
+    elif choice == "5":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice!")
+
         
 
